3ac09d999f8b2cd5c12b0ce8feecc3b52a0d20e2
sunbin0103 <sunbin0103@naver.com>
2019-03-16 21:36:52 +0900

    [QUEUE_PROBLEM] Add solution for tower
    - Adds solution for tower.

---
+++ 4th_week/elee/Tower.cpp
@@ -0,0 +1,41 @@
+﻿#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> heights) {
+    vector<int> answer;
+    queue<int> q;
+    int size = heights.size();
+ 
+    reverse(heights.begin(), heights.end());
+    
+    for (int i = 0; i < size; i++) 
+        q.push(heights[i]);
+ 
+    int key = size;
+    int pos = 1;
+    
+    while (!q.empty()) 
+    {
+        int cand = q.front(); 
+        q.pop();
+        
+        int i = 0;
+        
+        for (i = pos; i < size; i++) 
+        {
+            if (cand < heights[i]) 
+                break;
+        }
+        pos++;
+ 
+        answer.push_back(size - i);
+    }
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
